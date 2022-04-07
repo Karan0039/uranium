05416ea8ca6e9b7f39e9452182aff98867c03c1f
Karan039 <krndhamija9@gmail.com>
2022-04-07 21:37:54 +0530

    get_api

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,20 +3,74 @@ const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/user-profile/:abcd', function(req, res) {
+router.get('/user-profile/:abcd', function (req, res) {
     console.log(req)
     console.log(req.params.abcd)
     res.send('dummy response')
 })
+// Problem 1
+router.get('/movies', function (req, res) {
+    let arrMovies = ["Dune", "Uncharted", "Avengers", "RRR", "Knives Out"];
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+    res.send(arrMovies);
 });
 
+
+//Problem 2 & 3
+router.get('/movies/:indexNumber', function (req, res) {
+    let arrMovies = ["Dune", "Uncharted", "Avengers", "RRR", "Knives Out"];
+
+    if (req.params.indexNumber >= 0 && req.params.indexNumber <= arrMovies.length)
+        res.send(arrMovies[req.params.indexNumber]);
+    else res.send('use a valid index number');
+});
+
+
+//Problem 4
+router.get('/films', function (req, res) {
+    let arrMovies = [{
+        "id": 1,
+        "name": "The Shining"
+    }, {
+        "id": 2,
+        "name": "Incendies"
+    }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+    }, {
+        "id": 4,
+        "name": "Finding Nemo"
+    }];
+
+
+    res.send(arrMovies);
+});
+
+
+// Problem 5
+router.get('/films/:indexNumber', function (req, res) {
+    let arrMovies = [{
+        "id": 1,
+        "name": "The Shining"
+    }, {
+        "id": 2,
+        "name": "Incendies"
+    }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+    }, {
+        "id": 4,
+        "name": "Finding Nemo"
+    }];
+    let objMovie;
+    if (req.params.indexNumber >= 0 && req.params.indexNumber <= arrMovies.length) {
+        for (let i = 0; i < arrMovies.length; i++) {
+            if (arrMovies[i].id == req.params.indexNumber)
+            res.send(arrMovies[i]);
+        }
+    }
+    else res.send('No movie exists with this id'); 
+});
 
 
 
