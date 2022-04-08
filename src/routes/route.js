3c9c6b9fc06cc6574a202d9e839c19ef2339a1ef
Karan039 <krndhamija9@gmail.com>
2022-04-08 21:05:24 +0530

    post-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,16 +1,61 @@
 const express = require('express');
-const missing = require('./missingNumber')
-
 
 const router = express.Router();
 
-router.get('/prob1', function (req, res) {
-    res.send(missing.prob1());
+let players =
+[
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": [
+            "swimming"
+        ]
+    },
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": [
+            "soccer"
+        ]
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ]
+    },
+]
+
+router.post('/players', function (req, res) {
+    let data = req.body;
+    let temp=0;
+    for(let i=0;i<players.length;i++){
+    if(players[i].name==data.name)
+        res.send("Error: player is already present, enter new data!!")
+        else temp++;
+    }
+    if(temp>0){
+        players.push(data);
+    }
+    res.send(players);
 });
 
-router.get('/prob2', function (req, res) {
-    res.send(missing.prob2());
-    });
 
+
+
+
+    router.post('/post1', function (req, res) {
+        let data=req.body;
+        console.log(data);
+        res.send({msg:"This is my post api"});
+        });
+    
 module.exports = router;
 // adding this comment for no reason
